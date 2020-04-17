df2c6488165fcc234f43fab1ae42873830535c7d
Burcu Erdoğan <burcerdgn@gmail.com>
2020-04-17 14:57:56 +0300

    Notificaiton Window tried.

---
+++ TimerApp/Notification1.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Security.Principal;
+
+namespace TimerApp
+{
+    /// <summary>
+    /// Interaction logic for Notification1.xaml
+    /// </summary>
+    public partial class Notification1 : Window
+    {
+        private Notification1 notification1;
+        public Notification1()
+        {
+            InitializeComponent();
+        }
+
+       /* private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            notification1 = new Notification1();
+            notification1.Show();
+            this.Close();
+        }*/
+    }
+}
