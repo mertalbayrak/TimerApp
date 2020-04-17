c2b21cb2e975310c1c0a9d79ea58a6981e6d7e08
Mehmet Saygılı <mehmet.saygili@softtech.com.tr>
2020-04-17 15:05:36 +0300

    Window application

--- TimerApp/TimerWindow.xaml.cs
+++ TimerApp/TimerWindow.xaml.cs
@@ -62,10 +62,11 @@ namespace TimerApp
                 this.popup.IsOpen = true; //opens our custom msgbox
 
            //     if (this.popup.IsOpen == true)
-             //   {
-               //     scrollviewerCustom.VerticalScrollBarVisibility = ScrollBarVisibility.Disabled; //disables the scrollviewer
-                 //   this.ApplicationBar.Disable(); //disables the application bar
-              //  }
+           //     {
+           //         scrollviewerCustom.VerticalScrollBarVisibility = ScrollBarVisibility.Disabled; //disables the scrollviewer
+          //          this.ApplicationBar.Disable(); //disables the application bar
+           //     }
+
 
                 MessageBox.Show("Mola vermen gerekiyor :)" + sectorList);
 
@@ -73,12 +74,12 @@ namespace TimerApp
             }
         }
 
-        //on click of OK button
+         //on click of OK button
         private void OKButton_Click(object sender, RoutedEventArgs e)
         {
             this.popup.IsOpen = false; //closes our msgbox
-        //    scrollviewerCustom.VerticalScrollBarVisibility = ScrollBarVisibility.Visible; //enables the scrollviewer
-         //  this.ApplicationBar.Enable(); //enables the application bar
+        //            scrollviewerCustom.VerticalScrollBarVisibility = ScrollBarVisibility.Visible; //enables the scrollviewer
+        //            this.ApplicationBar.Enable(); //enables the application bar
         }
 
         private string GetTimerFormat(DateTime dateTime)
